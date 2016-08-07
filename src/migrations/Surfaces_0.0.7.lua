d11b54a0934fb6b43528b12c15f0991e0401af34
Simcra <Simcra@live.com>
2016-08-07 18:01:28 +0930

    attempted fix for migrations

--- src/migrations/Surfaces_0.0.7.lua
+++ src/migrations/Surfaces_0.0.7.lua
@@ -2,6 +2,4 @@ require("script.lib.api")
 require("script.lib.surfaces")
 
 game.reload_script()
-for k, v in pairs(api.game.surfaces()) do
-	surfaces.migrate_surface(v, "_")
-end
+global.surface_migration_separator_string = "_"
